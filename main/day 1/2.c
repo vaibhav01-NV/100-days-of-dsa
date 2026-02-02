af4c66e5a4fd266d74b226fd070dfa255f4bf4f4
Vaibhav Kumar <vaibhavk7181@gmail.com>
2026-02-02 20:42:59 +0530

    .

---
+++ main/day 1/2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, pos;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &pos);
+
+    // Shift elements to the left
+    for (int i = pos - 1; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    // Print updated array
+    for (int i = 0; i < n - 1; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
